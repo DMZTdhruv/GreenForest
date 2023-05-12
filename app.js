abdef7502d656045c300f3afd0f2a9f73d94d544
DMZTdhruv <125784252+DMZTdhruv@users.noreply.github.com>
2023-05-12 14:13:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// navbar
+let navBarMenu = document.querySelector('.navsecond-part');
+console.log(navBarMenu)
+let navBarMenuBtn = document.querySelector('.toggle-nav');
+
+const toggleNavOnClick = () => {
+    const visibility = navBarMenu.getAttribute('data-visible');
+    console.log(visibility);
+    if(visibility == "false"){
+        navBarMenu.setAttribute('data-visible',"true");
+        navBarMenu.setAttribute('aria-expanded',"true");
+        navBarMenuBtn.innerHTML = '<h3 class="close"> X </h3>';
+    } else if(visibility == "true") {
+        navBarMenu.setAttribute('data-visible',"false");
+        navBarMenu.setAttribute('aria-expanded',"false");
+        navBarMenuBtn.innerHTML = "|||";
+    }
+}
+
+navBarMenuBtn.addEventListener('click',toggleNavOnClick);
+
+// accordian
+let btn = document.querySelectorAll('.accordian-trigger');
+let accordianContent = document.querySelectorAll('.accordian-content');
+console.log(btn,accordianContent);
+
+btn.forEach((_button,index)=>{
+    btn[index].addEventListener('click',()=>{
+        btn[index].classList.toggle('open')
+        accordianContent[index].classList.toggle('active');
+    })
+})
